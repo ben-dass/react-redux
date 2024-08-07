7ac0b4173b9d2a986ba662f6588820aa502a359e
Benjamin Dass <benrdass@gmail.com>
2024-08-07 08:15:34 -0400

    init2

---
+++ src/reducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+
+import filtersReducer from "./features/filters/filtersSlice";
+import todosReducer from "./features/todos/todoSlice";
+
+const rootReducer = combineReducers({
+	todos: todosReducer,
+	filters: filtersReducer,
+});
+
+export default rootReducer;
